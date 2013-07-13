6b6a3c53b3e5edc73eaab5194af7140dc8f6e522
Carlos Santana <csantana23@gmail.com>
2013-07-13 17:12:29 -0400

    Made jslint compatible

--- src/app/main.js
+++ src/app/main.js
@@ -1,3 +1,6 @@
+/*global define, console*/
+/*jslint todo: true */
+
 /**
  * This file is the application's main JavaScript file. It is listed as a dependency in run.js and will automatically
  * load when run.js loads.
@@ -17,37 +20,36 @@
  * More information about everything described about the loader throughout this file can be found at
  * <http://dojotoolkit.org/reference-guide/loader/amd.html>.
  */
-define([ 
-	'dojo/sniff',
-	'require',
-	'dojox/app/main',
-	'dojox/json/ref',
-	'dojo/text!app/config.json',
-	//TODO: add all html templates being use in config.json to force them to be included in layer
-	// when doing custom dojo build, the build process will recognize them as dependencies for the package
-	'dojo/text!app/views/app.html',
-	'dojo/text!app/views/view1/view1.html',
-	'dojo/domReady!'
+define([
+    'dojo/sniff',
+    'require',
+    'dojox/app/main',
+    'dojox/json/ref',
+    'dojo/text!app/config.json',
+    //TODO: add all html templates being use in config.json to force them to be included in layer
+    // when doing custom dojo build, the build process will recognize them as dependencies for the package
+    'dojo/text!app/views/app.html',
+    'dojo/text!app/views/view1/view1.html',
+    'dojo/domReady!'
 ], function (has, require, Application, json, config) {
+    'use strict';
+    /**
+     * This main.js file conditionally executes different code depending upon the host environment it is loaded in.
+     * This is an increasingly common pattern when dealing with applications that run in different environments that
+     * require different functionality (i.e. client/server or desktop/tablet/phone).
+     */
+    if (has('host-browser')) {
+        var myapp;
 
-	/**
-	 * This main.js file conditionally executes different code depending upon the host environment it is loaded in.
-	 * This is an increasingly common pattern when dealing with applications that run in different environments that
-	 * require different functionality (i.e. client/server or desktop/tablet/phone).
-	 */
-	if (has('host-browser')) {
- 		 
+            // populate has flag on whether html5 history is correctly supported or not
+        has.add("html5history", !has("ie") || has("ie") > 9);
 
-       		// populate has flag on whether html5 history is correctly supported or not
-		has.add("html5history", !has("ie") || has("ie") > 9);		    
+        myapp = new Application(json.fromJson(config));
 
-		Application(json.fromJson(config));
-			
 
-      
-	}
-	else {
-		// TODO: Eventually, the Boilerplate will actually have a useful server implementation here :)
-		console.log('Hello from the server!');
-	}
+
+    } else {
+        // TODO: Eventually, the Boilerplate will actually have a useful server implementation here :)
+        console.log('Hello from the server!');
+    }
 });
