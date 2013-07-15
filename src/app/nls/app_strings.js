37dbdfabd29414054f460352ba55e27d25fd8337
Carlos Santana <csantana23@gmail.com>
2013-07-15 16:35:56 -0400

    Add nls support for Theme selector

--- src/app/nls/app_strings.js
+++ src/app/nls/app_strings.js
@@ -3,7 +3,9 @@ define({
     root: {
         app_string: 'This text from App level nls strings app/nls/app_strings.js',
         app_template_title: 'Template from App Level',
-        locale_link: '?locale=es-es'
+        locale_link: '?locale=es-es',
+        switch_theme: 'Switch Theme',
+        custom: 'Custom'
     },
     es: true
 });
--- src/app/nls/es/app_strings.js
+++ src/app/nls/es/app_strings.js
@@ -2,5 +2,7 @@
 define({
     app_string: 'Este texto es al nivel de la Aplicación en el archivo app/nls/es/app_strings.js',
     app_template_title: 'Plantilla al nivel de Aplicación',
-    locale_link: '?locale=en-us'
+    locale_link: '?locale=en-us',
+    switch_theme: 'Cambia Estilo',
+    custom: 'Personalizado'
 });
