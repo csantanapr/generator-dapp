55eddeadb9c861ea024e0d432e8dada4be356955
Carlos Santana <csantana23@gmail.com>
2013-07-15 15:58:09 -0400

    Added optimized build output as pre-production

---
+++ mobile/www/app/nls/main_es-es.js.uncompressed.js
@@ -0,0 +1,5 @@
+define('app/nls/main_es-es',{
+'app/nls/app_strings':{"app_string":"Este texto es al nivel de la Aplicación en el archivo app/nls/es/app_strings.js","app_template_title":"Plantilla al nivel de Aplicación","locale_link":"?locale=en-us"}
+,
+'app/views/view1/nls/view1_strings':{"view1_string":"Este texto es al nivel de la Pagina/Vista en el archivo nls","view1_template_title":"Plantilla al nivel de la Vista"}
+});
