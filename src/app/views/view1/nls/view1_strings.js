9b5a6063e963204ace3ccd5639288d2d465e7ba6
Carlos Santana <csantana23@gmail.com>
2013-07-18 15:51:16 -0400

    Added view nls file location

--- src/app/views/view1/nls/view1_strings.js
+++ src/app/views/view1/nls/view1_strings.js
@@ -1,7 +1,7 @@
 /*global define*/
 define({
     root: {
-        view1_string: 'This text from View level nls strings',
+        view1_string: 'This text from View level nls strings app/views/view1/nls/es/view1_string.js',
         view1_template_title: 'Template at View level'
     },
     es: true
