52b868560e2b6bcffb54e9c65d826d014fbb4da7
Carlos Santana <csantana23@gmail.com>
2013-07-18 15:53:40 -0400

    updated nls files

--- dist/www/app/nls/main_ar.js
+++ dist/www/app/nls/main_ar.js
@@ -1,3 +1,3 @@
 //>>built
-define("app/nls/main_ar",{"app/nls/app_strings":{app_string:"This text from App level nls strings app/nls/app_strings.js",app_template_title:"Template from App Level",locale_link:"?locale\x3des-es",switch_theme:"Switch Theme",custom:"Custom"},"app/views/view1/nls/view1_strings":{view1_string:"This text from View level nls strings",view1_template_title:"Template at View level"}});
+define("app/nls/main_ar",{"app/nls/app_strings":{app_string:"This text from App level nls strings app/nls/app_strings.js",app_template_title:"Template from App Level",locale_link:"?locale\x3des-es",switch_theme:"Switch Theme",custom:"Custom"},"app/views/view1/nls/view1_strings":{view1_string:"This text from View level nls strings app/views/view1/nls/es/view1_string.js",view1_template_title:"Template at View level"}});
 //@ sourceMappingURL=main_ar.js.map
