ac3b85232e81316bf1e12a40141a5b74b10e2d6c
Carlos Santana <csantana23@gmail.com>
2013-08-19 15:16:49 -0400

    Implemented dojo build with Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,6 +23,9 @@ module.exports = function (grunt) {
             }
         },
         watch: {
+            options: {
+                livereload: true
+            },
             gruntfile: {
                 files: '<%= jshint.gruntfile.src %>',
                 tasks: ['jshint:gruntfile', 'jslint:gruntfile']
@@ -30,6 +33,7 @@ module.exports = function (grunt) {
             src: {
                 files: '<%= jshint.src.src %>',
                 tasks: ['jshint:src', 'jslint:src']
+
             }
         },
         dojo: {
@@ -45,6 +49,119 @@ module.exports = function (grunt) {
                 dojo: 'components/dojo/dojo.js', // Path to dojo.js file in dojo source
                 load: 'build' // Optional: Utiltbootstrap (Default:
             }
+        },
+        copy: {
+            app: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'src',
+                        src: ['index.html'],
+                        dest: 'dist/www/'
+                    }
+                ]
+            },
+            app_css: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'dist/.build',
+                        src: ['app/views/css/app.css'],
+                        dest: 'dist/www/'
+                    }
+                ]
+            },
+            app_images: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'dist/.build',
+                        src: ['app/views/images/**'],
+                        dest: 'dist/www/'
+                    }
+                ]
+            },
+            app_js: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'dist/.build',
+                        src: ['app/main.js'],
+                        dest: 'dist/www/'
+                    }
+                ]
+            },
+            app_nls: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'dist/.build',
+                        src: ['app/main*.js'],
+                        dest: 'dist/www/'
+                    }
+                ]
+            },
+            app_data: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'dist/.build',
+                        src: ['app/resources/data/**'],
+                        dest: 'dist/www/'
+                    }
+                ]
+            },
+            app_view1: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'dist/.build',
+                        src: ['app/views/view1/images/**'],
+                        dest: 'dist/www/'
+                    }
+                ]
+            },
+            dojo_themes: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'dist/.build',
+                        src: [
+                            'dojox/mobile/themes/android/android.css',
+                            'dojox/mobile/themes/iphone/iphone.css',
+                            'dojox/mobile/themes/iphone/ipad.css',
+                            'dojox/mobile/themes/blackberry/blackberry.css',
+                            'dojox/mobile/themes/holodark/holodark.css',
+                            'dojox/mobile/themes/windows/windows.css',
+                            'dojox/mobile/themes/custom/custom.css'
+                        ],
+                        dest: 'dist/www/'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'dist/.build',
+                        src: [
+                            'dojox/mobile/themes/android/images/**',
+                            'dojox/mobile/themes/iphone/images/**',
+                            'dojox/mobile/themes/blackberry/images/**',
+                            'dojox/mobile/themes/holodark/images/**',
+                            'dojox/mobile/themes/windows/images/**',
+                            'dojox/mobile/themes/custom/images/**'
+                        ],
+                        dest: 'dist/www/'
+                    }
+                ]
+            },
+            dojo: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'dist/.build',
+                        src: ['dojo/dojo.js', 'build-report.txt'],
+                        dest: 'dist/www/'
+                    }
+                ]
+            }
         }
     });
     // These plugins provide necessary tasks.
@@ -52,8 +169,12 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-jslint');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-dojo');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     // Default task.
-    grunt.registerTask('default', ['jshint', 'jslint']);
+
+    grunt.registerTask('lint', ['jshint', 'jslint']);
+    grunt.registerTask('build', ['lint', 'dojo', 'copy']);
+    grunt.registerTask('default', ['build']);
 
 };
